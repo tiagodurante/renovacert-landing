624e233f33311431b16322420d3deab5b7a79626
Tiago Amado Durante <tiagodurante@outlook.com.br>
2022-04-11 00:04:27 -0300

    grid implemented

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -12,7 +12,7 @@ interface PriceProps {
 const Price: FC<PriceProps> = (props) => {
   return (
     <>
-      <div className={`bg-${props.bgColor} sm:px-8 py-10 text-center ${props.customClass}`}>
+      <div className={`bg-${props.bgColor} px-4 sm:px-8 py-10 text-center ${props.customClass}`}>
         <p className={`text-${props.txColor} uppercase text-lg sm:text-1xl lg:text-2xl font-bold`}>{props.title}</p>
         <div className={`text-${props.txColor} pb-6 pt-10 font-medium`}>R$<span className="text-5xl font-bold">{props.price}</span></div>
         {props.children}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import AppSection from '../components/Section'
 import AppPrice from '../components/Price'
+import AppColumn from '../components/Column'
 import "../styles/global.css";
 
 const IndexPage = () => {
@@ -23,20 +24,20 @@ const IndexPage = () => {
         <div className="text-center">
           <h3 className="text-black font-bold text-4xl sm:text-5xl">Uma assinatura digital para diversos serviços</h3>
         </div>
-        <div className={`sm:columns-3 md:py-16`}>
+        <AppColumn quantity={3}>
           <div className="text-center py-10">
-            <h5 className="text-2xl">Anti-spam</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <h5 className="text-2xl">Emissão de nota fiscal</h5>
+            <p>Realize a emissão de notas fiscais para sua empresa ou produtor rural</p>
           </div>
           <div className="text-center py-10">
-            <h5 className="text-2xl">Anti-spam</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <h5 className="text-2xl">Serviços do governo</h5>
+            <p>Acesse todos os serviços disponibilizados pelo governo federal e estadual de forma ilimitada com sua assinatura digital</p>
           </div>
           <div className="text-center py-10">
-            <h5 className="text-2xl">Anti-spam</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <h5 className="text-2xl">Assinatura digital</h5>
+            <p>Assine documentos digitais de forma segura</p>
           </div>
-        </div>
+        </AppColumn>
       </AppSection>
       <AppSection bgColor="white">
         <div className="text-center">
@@ -48,30 +49,37 @@ const IndexPage = () => {
       <AppSection bgColor="white">
         <div className="text-center">
           <h3 className="text-black font-bold text-4xl">Atendimento a distância</h3>
-          <p className="text-black mt-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla impedit aliquid mollitia, ea dignissimos a iure libero quisquam blanditiis incidunt eius ratione praesentium tenetur voluptatem dolor cupiditate assumenda porro eveniet.</p>
+          <p className="text-black mt-8">Oferecemos este serviço para o cliente que não tem disponibilidade para realizar o modo presencial, permitindo com que todo o atendimento seja feito por videoconferência. Com o celular em mãos, a validação do documento com foto será realizada e o certificado poderá ser emitido dentro de 24 horas.</p>
           <button className="bg-background mt-8 text-text py-4 px-8 font-bold rounded-full">Learn More</button>
         </div>
       </AppSection>
       <AppSection bgColor="white">
         <div className="text-center">
-          <h3 className="text-black font-bold text-4xl sm:text-5xl">Check our pricing</h3>
+          <h3 className="text-black font-bold text-4xl sm:text-5xl">Consulte nossos preços</h3>
         </div>
-        <div className={`sm:columns-2 md:py-16`}>
+        <AppColumn quantity={2}>
           <AppPrice bgColor="white" txColor="background" title="Pessoa Física" price={140}>
-            <p>beneficio 1</p>
-            <p>beneficio 2</p>
-            <p>beneficio 3</p>
-            <p>beneficio 4</p>
+            <p>e-CPF A1 (1 ano)</p>
+            <p>e-CPF A3 Token (1 a 3 anos)</p>
+            <p>e-CPF A3 Cartão (1 a 3 anos)</p>
+            <p className="font-bold mt-4">Documentos necessários</p>
+            <p>Documento com foto</p>
+            <p>Comprovante de endereço</p>
+            <p>E-mail e telefone para contato</p>
             <button className="bg-text border-black mt-12 text-black border-2 py-4 px-8 font-bold rounded-full">Learn More</button>
           </AppPrice>
-          <AppPrice bgColor="background" txColor="text" title="Pessoa Jurídica" price={180} customClass="rounded-md">
-            <p className="text-text">beneficio 1</p>
-            <p className="text-text">beneficio 2</p>
-            <p className="text-text">beneficio 3</p>
-            <p className="text-text">beneficio 4</p>
+          <AppPrice bgColor="background" txColor="text" title="Pessoa Jurídica em Geral" price={180} customClass="rounded-md">
+            <p className="text-text">e-CNPJ A1 (1 ano)</p>
+            <p className="text-text">e-CNPJ A3 Token (1 a 3 anos)</p>
+            <p className="text-text">e-CNPJ A3 Cartão (1 a 3 anos)</p>
+            <p className="text-text font-bold mt-4">Documentos necessários</p>
+            <p className="text-text">Contrato social, requerimento de empresário ou ata e estatuto atualizado</p>
+            <p className="text-text">Documento com foto</p>
+            <p className="text-text">Comprovante de endereço</p>
+            <p className="text-text">E-mail e telefone para contato</p>
             <button className="bg-secondary mt-12 text-black py-4 px-8 font-bold rounded-full">Learn More</button>
           </AppPrice>
-        </div>
+        </AppColumn>
       </AppSection>
       <AppSection bgColor="background">
         <div className="text-center text-text">
@@ -79,8 +87,9 @@ const IndexPage = () => {
         </div>
       </AppSection>
       <AppSection bgColor="background">
-        <div className={`sm:columns-2 md:py-16`}>
-          <div className="h-fit text-text">
+        <AppColumn quantity={2}>
+        {/* <div className={`grid sm:grid-cols-2 md:py-16`}> */}
+          <div className="text-text">
             <h3 className="text-4xl font-bold mb-8">Contact Us</h3>
             <p className="mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum commodi quod sunt alias suscipit saepe aspernatur excepturi nesciunt unde, reiciendis quam illo explicabo dignissimos soluta incidunt obcaecati voluptatum ipsam modi.</p>
             <p className="mb-1">email: tiagodurante@outlook.com.br</p>
@@ -94,7 +103,7 @@ const IndexPage = () => {
             <p className="mb-1">phone: 44998922501</p>
             <p className="mb-1">endereco: rua 12 de dezembro, 242, centro</p>
           </div>
-        </div>
+        </AppColumn>
       </AppSection>
       <AppSection bgColor="background">
         <div className="flex flex-row justify-between">
